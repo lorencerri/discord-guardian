bac32b732ebafd49ed4e892b6433ec4b2004d392
TrueXPixels <truexpixels@gmail.com>
2020-04-08 11:59:16 -0700

    Remove the now ignored directory '.vs/'

--- index.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-const GuardianClient = require('./core/client.js');
-const client = new GuardianClient();
-
-client.login(process.env.BOT_TOKEN);
--- util/toProperCase.js
+++
@@ -1,7 +0,0 @@
- function toProperCase(str) {
-    str = str.split('_');
-    str = str.map(i => i.charAt(0).toUpperCase() + i.substring(1));
-    return str.join(' ');
-};
-
-module.exports = toProperCase;
