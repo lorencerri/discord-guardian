1705436eca76ef666a8d61ba25ea9aa1c951e85b
TrueXPixels <truexpixels@gmail.com>
2020-04-09 01:26:05 -0700

    Modify recent.js

--- commands/guardian/recent.js
+++ commands/guardian/recent.js
@@ -18,12 +18,10 @@ class RecentCommand extends Command {
 
     async exec(message, args) {
 
-        let lines = [`*You can do **\`${message.guild.prefix}recent <ID>\`** to view all actions relating to a user.*`]
-
         const embed = this.client.util.embed()
             .setColor(0x7289DA)
             .setTitle(`Recent Actions in ${message.guild.name}`)
-            .setDescription(lines.join('\n'));
+            .setDescription(`*You can do **\`${message.guild.prefix}recent <ID>\`** to view all actions relating to a user.*`);
 
         let actions = message.guild.getActions(args.ID ? (i => i.executor.id === args.ID || (i.target && (i.target.id === args.ID))) : undefined);
         for (var k in actions) embed.addField(`${actions[k].name} (${(actions[k].actions || '').split('\n').length - 1})`, actions[k].actions || 'No entries.');
