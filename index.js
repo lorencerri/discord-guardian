c5526ab1ac7dbfa02823163f39e0d41b504f9c41
Loren Cerri <lorencerri@gmail.com>
2021-08-18 00:11:16 -0400

    Handle invalid .env files

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Guardian.
+require('dotenv').config();
+
+const Logger = require('@ayanaware/logger');
+const fs = require('fs');
+
+const log = Logger.get();
+
+// Preflight Checklist
+log.info('Starting preflight checklist...');
+
+if (!fs.existsSync('.env')) {
+	log.warn('The .env file does not exist');
+	log.warn('Creating an .env file');
+	fs.writeFileSync('.env', 'BOT_TOKEN=');
+	log.warn('Please fill out the required information in the .env file');
+	return log.warn('Exiting...');
+}
+
+if (!process.env.BOT_TOKEN)
+	return log.error('Discord Token not found in .env file!');
---
+++ issues.md
@@ -0,0 +1,5 @@
+# Common Issues
+
+## "Invalid Token"
+
+There was either no token created in the `.env` file, or the token provided was invalid.
