087b360cf0ed78ec11c4ba3de830e16e4b2b284e
Loren Cerri <lorencerri@gmail.com>
2022-02-02 03:16:04 -0500

    Start Discord Bot

---
+++ src/client.js
@@ -0,0 +1,19 @@
+import { GatewayIntentBits } from 'discord-api-types/v9'
+import { SapphireClient } from '@sapphire/framework'
+import { Constants } from 'discord.js'
+
+const init = async () => {
+
+	// Verify .env
+
+	// Initialize Client
+
+	// Attempt Logging In
+
+	// Catch Errors
+
+	// Initialize Database
+
+}
+
+export { init }
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// Start
+
+// Go to client.js
