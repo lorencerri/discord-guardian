d21c979f62da6deb9e1ec3aa80f70b91f8f364d4
Loren Cerri <lorencerri@gmail.com>
2022-02-02 02:51:11 -0500

    Complete setup file

--- setup.js
+++ setup.js
@@ -1,7 +1,36 @@
-console.log('Setup Script');
+import dotenv from 'dotenv';
+const { parsed, error: envError } = dotenv.config();
 
-// Check if modules are installed
+import chalk from 'chalk';
+import prompt from 'prompt-async';
 
-// Run setup
+import Keyv from 'keyv';
+const keyv = new Keyv('sqlite://database.sqlite');
 
-// Check if setup was properly completed
+const error = (message = '') => {
+	console.log(`${chalk.yellow(`[Setup]`)} ${chalk.red(`[ERROR] ${message}`)}`);
+}
+
+const info = (message = '') => {
+	console.log(`${chalk.yellow(`[Setup]`)} ${chalk.blue(`[INFO] ${message}`)}`);
+}
+
+const setup = async () => {
+	info(`Initializing Setup...`);
+	prompt.start();
+
+	if (envError) return error('Error loading .env file. Does it exist?');
+	if (!parsed["DISCORD_TOKEN"]) return error('DISCORD_TOKEN not found in .env file. Does it exist?');
+
+	const { owner_id } = await prompt.get([{
+		name: 'owner_id',
+		description: 'What is your Discord User ID? (This will be used to bypass any action limits) [Optional]'
+	}]);
+	if (owner_id) await keyv.set('owner_id', owner_id);
+
+	return 200;
+}
+
+const resp = await setup();
+if (resp !== 200) error('Setup failed. If you need assistance, create an issue on the GitHub repo!')
+else info('Setup completed without any errors!');
