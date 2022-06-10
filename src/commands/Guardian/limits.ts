b3e009035c52816d5e46ddaf62e6bd1d37cfe411
Loren Cerri <lorencerri@gmail.com>
2022-06-10 01:16:05 -0400

    Update limits.ts

--- src/commands/Guardian/limits.ts
+++ src/commands/Guardian/limits.ts
@@ -22,13 +22,19 @@ export class LimitsCommand extends Command {
 
 	registerApplicationCommands(registry: Command.Registry) {
 		registry.registerChatInputCommand((builder) => builder.setName('limits')
-			.setDescription('View or set interaction limits for the server')
-			.addSubcommand(subcommand => subcommand.setName('update').setDescription('Update the limit for a specific action')
+			.setDescription('View, add, or remove an interaction limit for the server')
+			.addSubcommand(subcommand => subcommand.setName('add').setDescription('Add a limit for a specific action')
 				.addStringOption(option => //
 					option.setName('action').setDescription('The name of the action to update').setRequired(true).setAutocomplete(true))
 				.addStringOption(option => //
-					option.setName('limit').setDescription('The new amount of times this action can be performed in a minute').setRequired(true).setAutocomplete(true)))
-			.addSubcommand(subcommand => subcommand.setName('view').setDescription('Returns all of the current limits')),
+					option.setName('limit').setDescription('The new amount of times this action can be performed in an interval').setRequired(true))
+				.addStringOption(option => //
+					option.setName('interval').setDescription('The interval for the action limit').setRequired(true).setAutocomplete(true))
+			)
+			.addSubcommand(subcommand => subcommand.setName('view').setDescription('Returns all of the current limits'))
+			.addSubcommand(subcommand => subcommand.setName('remove').setDescription('Returns all of the current limits')
+				.addStringOption(option => //
+					option.setName('limit').setDescription('The specific limit to remove').setRequired(true).setAutocomplete(true))),
 			{ behaviorWhenNotIdentical: RegisterBehavior.Overwrite }
 		);
 	}
